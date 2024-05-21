a221f76a6a5215b390c70be4c8f5f997d105bb7a
ifnesi <inesi@confluent.io>
2024-05-21 11:12:07 +0100

    Changed Coordinates to pydantic BaseModel

--- src/utils/gps.py
+++ src/utils/gps.py
@@ -1,18 +1,15 @@
 import math
 
+from pydantic import BaseModel
 
-class Coordinate:
-    def __init__(
-        self,
-        lat: float,
-        lon: float,
-    ) -> None:
-        """Latitude and Longitude in Decimal Degrees"""
-        self.lat = lat
-        self.lon = lon
+
+class Coordinate(BaseModel):
+    lat: float
+    lon: float
 
 
 class Coordinates:
+    # Earth radius in metres
     R = 6372797.6
 
     def destination(
@@ -31,16 +28,23 @@ class Coordinates:
         :return:
         - Latitude and Longitude (Coordinate)
         """
+
         bearing_rad = bearing * math.pi / 180
         lat_rad = coordinate.lat * math.pi / 180
         lon_rad = coordinate.lon * math.pi / 180
         angular_distance = distance / Coordinates.R
+
         lat = math.asin(
             math.sin(lat_rad) * math.cos(angular_distance)
             + math.cos(lat_rad) * math.sin(angular_distance) * math.cos(bearing_rad)
         )
+
         lon = lon_rad + math.atan2(
             math.sin(bearing_rad) * math.sin(angular_distance) * math.cos(lat_rad),
             math.cos(angular_distance) - math.sin(lat_rad) * math.sin(lat),
         )
-        return Coordinate(lat * 180 / math.pi, lon * 180 / math.pi)
+
+        return Coordinate(
+            lat=round(lat * 180 / math.pi, 8),
+            lon=round(lon * 180 / math.pi, 8),
+        )
